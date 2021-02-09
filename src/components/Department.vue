bcf09cf45261ec592bcbb69f9c4522b44f050544
Jopher Reyes <jopher.reyes@lorma.edu>
2021-02-09 14:50:28 +0800

    wala

--- src/components/Department.vue
+++ src/components/Department.vue
@@ -1,15 +1,16 @@
 <template>
    <div class="flex justify-center items-center h-screen">   
-    <div class="flex flex-col w-1/3">
-            <div class="flex flex-wrap bg-gray-400 p-8 justify-start">
-              <label for="department" class="w-full text-left"> Department </label>
-              <input type="text" id="department" class="w-full border-gray-400">
-            </div>                            
-            <div class="flex p-2 bg-gray-300 justify-end">
-                <button class="bg-blue-600 text-gray-50 rounded-md p-2"> Create  Department </button>
-             </div>                
-    </div>      
+                    <div class="flex flex-col w-1/3">
+                            <div class="flex flex-wrap bg-gray-400 p-8 justify-start">
+                                <label for="department" class="w-full text-left"> Offices </label>
+                                <input type="text" id="department" class="w-full border-gray-400">
+                            </div>                            
+                            <div class="flex p-2 bg-gray-300 justify-end">
+                                <button class="bg-blue-600 text-gray-50 rounded-md p-2"> Create  Department </button>
+                            </div>                
+                    </div>      
  </div>  
+
 </template>
 
 <script>
