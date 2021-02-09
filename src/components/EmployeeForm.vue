df400963df0a11c7c61f62ac09da5dcf07eafa46
Jopher Reyes <jopher.reyes@lorma.edu>
2021-02-09 16:36:04 +0800

    Res

--- src/components/EmployeeForm.vue
+++ src/components/EmployeeForm.vue
@@ -6,26 +6,26 @@
                 <div class="flex flex-wrap justify-start">            
                     <div>
                         <div class="p6">
-                            <label for="department" class=" w-full md:w-1/2 ld:w-1/3 text-left text-blue-600 text-lg"> Organization /  </label>            
-                            <label for="department" class=" w-full md:w-1/2 ld:w-1/3 text-left"> Murazik PLC Toy-O'Kon </label>            
+                            <label for="department" class=" w-full md:w-1/2 lg:w-1/3 text-left text-blue-600 text-lg"> Organization /  </label>            
+                            <label for="department" class=" w-full md:w-1/2 lg:w-1/3 text-left"> Murazik PLC Toy-O'Kon </label>            
                         </div>
                 </div>  
                         <div>
                         
                             <div>
                                 <div class="flex bg-gray-100 p-2">
-                                    <label for="department" class="w-full md:w-1/2 ld:w-1/3 text-left text-red-300">Basic Information</label>
+                                    <label for="department" class="w-full md:w-1/2 lg:w-1/3 text-left text-red-300">Basic Information</label>
                                 </div>
                                 <div class="flex flex-row">       
                                     <div>                                
                                         <div class="flex flex-wrap bg-gray-100 p-8 justify-start">                                            
-                                            <label for="department" class="w-full md:w-1/2 ld:w-1/3 text-left">First Name: </label>
-                                            <input type="text" id="department" class="w-full md:w-1/2 ld:w-1/3 border-gray-400">
+                                            <label for="department" class="w-full md:w-1/2 lg:w-1/3 text-left">First Name: </label>
+                                            <input type="text" id="department" class="w-full border-gray-400">
                                         </div>                            
                                         
                                         <div class="flex flex-wrap bg-gray-100 p-8 justify-start">                                
-                                            <label for="department" class="w-full md:w-1/2 ld:w-1/3 text-left">Email: </label>
-                                            <input type="text" id="department" class="w-full md:w-1/2 ld:w-1/3 border-gray-400">
+                                            <label for="department" class="w-full md:w-1/2 lg:w-1/3 text-left">Email: </label>
+                                            <input type="text" id="department" class="w-full border-gray-400">
                                         </div>                            
                                         
                                         <div class="flex p-2 bg-gray-300 justify-end">
@@ -34,13 +34,13 @@
                                     </div>    
                                     <div>
                                         <div class="flex flex-wrap bg-gray-100 p-8 justify-start">                                    
-                                            <label for="department" class="w-full md:w-1/2 ld:w-1/3 text-left">Last Name: </label>
-                                            <input type="text" id="department" class="w-full md:w-1/2 ld:w-1/3 border-gray-400">
+                                            <label for="department" class="w-full md:w-1/2 lg:w-1/3 text-left">Last Name: </label>
+                                            <input type="text" id="department" class="w-full border-gray-400">
                                         </div>                            
                                         
                                         <div class="flex flex-wrap bg-gray-100 p-8 justify-start">                                
-                                            <label for="department" class="w-full md:w-1/2 ld:w-1/3 text-left">Department: </label>
-                                            <input type="text" id="department" class="w-full md:w-1/2 ld:w-1/3 border-gray-400">
+                                            <label for="department" class="w-full md:w-1/2 lg:w-1/3 text-left">Department: </label>
+                                            <input type="text" id="department" class="w-full border-gray-400">
                                         </div>                            
                                         
                                         <div class="flex p-2 bg-gray-300 justify-end">
