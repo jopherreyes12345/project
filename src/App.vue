987514b88c0b6cc6c09d840f84ed20915acdcc3e
Jopher Reyes <jopher.reyes@lorma.edu>
2021-02-09 14:27:06 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <!--<marquee><img alt="Vue logo" src="./assets/logo.png"></marquee>-->
+    <HelloWorld msg="Department"/>
+  
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/Department.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  
+   
+  
+
+ <div class="flex justify-center items-center h-screen">   
+    <div class="flex flex-col w-1/3">
+            <div class="flex flex-wrap bg-gray-400 p-8 justify-start">
+              <label for="department" class="w-full text-left"> Department</label>
+              <input type="text" id="department" class="w-full border-gray-400">
+            </div>                            
+            <div class="flex p-2 bg-gray-300 justify-end">
+                <button class="bg-blue-600 text-gray-50 rounded-md p-2"> Create  Department </button>
+             </div>                
+    </div>      
+ </div>  
+
+   
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
